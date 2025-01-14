93103ee43d747c4267404283fb4008301cae34e6
Eylem Eryılmaz <eryilmazeylemmm@gmail.com>
2025-01-15 02:13:29 +0300

    Add project files.

---
+++ Veri_Tabanli_Parti_Secim_Grafik_Istatistik/FrmGrafikler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Veri_Tabanli_Parti_Secim_Grafik_Istatistik
+{
+    public partial class FrmGrafikler : Form
+    {
+        public FrmGrafikler()
+        {
+            InitializeComponent();
+        }
+    }
+}
